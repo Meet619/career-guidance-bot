7faac4a027915d1cf5ed4084ffcf7b24a015115e
MEET BHAVSAR <51154136+Meet619@users.noreply.github.com>
2020-03-21 14:59:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("chatterbot.corpus.english")
+
+
+# # Trial
+# bot = ChatBot('Career_Bot',storage_adapter="chatterbot.storage.SQLStorageAdapter")
+# trainer = ListTrainer(bot)
+# conv = open('Meet.txt','r').readlines()
+# trainer.train(conv)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
