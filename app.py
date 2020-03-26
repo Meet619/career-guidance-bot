c70c4cb54fc2322c9445cbd249e2ffdc3295fa6d
MEET BHAVSAR <51154136+Meet619@users.noreply.github.com>
2020-03-26 22:15:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(bot.get_response(userText))
+    return str(english_bot.get_response(userText))
 
 
 if __name__ == "__main__":
