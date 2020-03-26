d7958c1fe61f0566dfe75b6cebaa520d67f063a7
MEET BHAVSAR <51154136+Meet619@users.noreply.github.com>
2020-03-26 22:37:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
+#english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+#trainer = ChatterBotCorpusTrainer(english_bot)
+#trainer.train("chatterbot.corpus.english")
 
 
 # # Trial
-# bot = ChatBot('Career_Bot',storage_adapter="chatterbot.storage.SQLStorageAdapter")
-# trainer = ListTrainer(bot)
-# conv = open('Meet.txt','r').readlines()
-# trainer.train(conv)
+bot = ChatBot('Career_Bot',storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ListTrainer(bot)
+conv = open('Meet.txt','r').readlines()
+trainer.train(conv)
 
 
 @app.route("/")
@@ -26,7 +26,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    return str(bot.get_response(userText))
 
 
 if __name__ == "__main__":
